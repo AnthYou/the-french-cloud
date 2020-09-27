8568e1c01ec9bd82f2349ad3954fe792b814a36b
Anthony You <you.anthony@yahoo.com>
2020-09-27 17:46:51 +0200

    edited sign up and edit profile forms

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroying all instances..."
+User.destroy_all
+
+puts "Seeding users..."
+User.create!(first_name: 'Anthony', last_name: 'You', email: 'you.anthony@yahoo.com', password: '123456', birth_date: '01/31/1995'.to_date, country: 'France', level: 'C2', description: 'Entrepreneur and Web Developer')
+User.create!(first_name: 'Nelly', last_name: 'Frisicaro', email: 'nelly.frisicaro@gmail.com', password: '123456', birth_date: '04/16/1997'.to_date, country: 'France', level: 'C2', description: 'Lead Teacher')
