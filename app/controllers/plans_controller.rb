d4b3539993807f2273eae0ee2dbb0bc21951e138
Anthony You <you.anthony@yahoo.com>
2020-10-05 17:02:35 +0200

    edited show to display default card

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,5 +1,9 @@
 class PlansController < ApplicationController
   def show
     @plan = Plan.find(params[:id])
+    if current_user.subscribed?
+      subscription = Stripe::Subscription.retrieve(current_user.subscription_id)
+      @default_payment_method = Stripe::PaymentMethod.retrieve(subscription.default_payment_method)
+    end
   end
 end
