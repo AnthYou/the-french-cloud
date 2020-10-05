d0706888c95889cbea303518c60e6eefe9e1160f
Anthony You <you.anthony@yahoo.com>
2020-10-05 20:03:21 +0200

    added some front to the cards and fixed user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,20 @@
 class UsersController < ApplicationController
+  before_action :check_if_subscriber?, only: :index
+
   def index
     @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  private
+
+  def check_if_subscriber?
+    unless user_signed_in? && current_user.subscribed?
+      flash[:alert] = "You need to subscribe to a plan first."
+      redirect_to root_path
+    end
   end
 end
