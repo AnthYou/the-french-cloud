e3159e6e5cb34bd2349442304f22b15746f040bf
Anthony You <you.anthony@yahoo.com>
2020-10-06 20:28:30 +0200

    created about me

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   get 'testimonials', to: 'pages#testimonials', as: 'testimonials'
+  get 'about_me', to: 'pages#about_me', as: 'about_me'
 
   resources :users, only: [:index, :show]
 
