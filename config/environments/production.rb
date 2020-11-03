b32c7f70118f71ca5ed7558e8640a87522642a82
Anthony You <you.anthony@yahoo.com>
2020-11-03 16:01:53 +0100

    asset compile true in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # For video addition, no precompiled
   config.assets.check_precompiled_asset = false
