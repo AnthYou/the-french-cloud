a9811737e4aacc9bdf6c0224aef9f566bdfefc7a
Anthony You <you.anthony@yahoo.com>
2020-11-05 14:52:22 +0100

    added admin to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :check_if_subscriber?, only: :index
+  before_action :access_my_profile_only, only: :show
 
   def index
     @users = User.all.sort_by(&:first_name)
@@ -12,9 +13,16 @@ class UsersController < ApplicationController
   private
 
   def check_if_subscriber?
-    unless user_signed_in? && current_user.subscribed?
-      flash[:alert] = "You need to subscribe to a plan first."
-      redirect_to root_path
-    end
+    return if (user_signed_in? && current_user.subscribed?) || current_user.admin?
+
+    flash[:alert] = "You need to subscribe to a plan first."
+    redirect_to root_path
+  end
+
+  def access_my_profile_only
+    return if (User.find(params[:id]) == current_user) || current_user.subscribed? || current_user.admin?
+
+    flash[:alert] = "You need to subscribe to a plan first."
+    redirect_to root_path
   end
 end
